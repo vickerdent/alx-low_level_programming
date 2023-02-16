83f3bca74fda6c87458c72d0003b7e1a31c9ba67
Victor Abuka <vickerdent@gmail.com>
2023-02-16 17:11:25 +0000

    tired, of c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  /* Initialize string */
+char my_string[] = "with proper grammar, but the outcome is a piece of art,";
+
+  /* send my_string to stdout */
+printf (my_string);
+
+return (0);
+}

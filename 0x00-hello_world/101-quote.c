40cdc8f12c8bfafe61de44705e75f0f4b3e18bb2
Victor Abuka <vickerdent@gmail.com>
2023-02-16 19:00:40 +0000

    Print using write function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,13 @@ int write(int filedes, const char *buf, unsigned int nbyte);
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 
 int main(void)
 {
   /* send my_string to stdout */
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-return (0);
+return (1);
 }

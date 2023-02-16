aba88b1ee5bf5bd123cd4fb0370d7ab083281eab
Victor Abuka <vickerdent@gmail.com>
2023-02-16 17:40:31 +0000

    Print several sizes of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,11 +15,11 @@ long long int longlongType;
 float floatType;
 
   /* send data to stdout */
-printf("Size of a char: %ld byte(s)\n",sizeof(charType));
-printf("Size of an int: %ld byte(s)\n",sizeof(integerType));
-printf("Size of a long int: %ld byte(s)\n",sizeof(longType));
-printf("Size of a long long int: %ld byte(s)\n",sizeof(longlongType));
-printf("Size of a float: %ld byte(s)\n",sizeof(floatType));
+printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+printf("Size of an int: %ld byte(s)\n", sizeof(integerType));
+printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongType));
+printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 
 return (0);
 }

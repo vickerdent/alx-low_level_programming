7dd01ccbfe01fda13bff330e9b15a9d4f502d952
Victor Abuka <vickerdent@gmail.com>
2023-02-16 16:37:37 +0000

    Finally did my first C code within Linux

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  /* Initialize string */
+  char my_string[] = "\"Programming is like building a multilingual puzzle";
+
+    /* send my_string to stdout */
+  puts(my_string);
+
+  return 0;
+}

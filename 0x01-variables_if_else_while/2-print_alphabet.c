5fd966c7f7a18cb7237d3d51864fca6ae7781c03
Victor Abuka <vickerdent@gmail.com>
2023-02-17 13:30:59 +0000

    Use putchar(), not printf or any other

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c = '\n';
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar(c);
+return (0);
+}

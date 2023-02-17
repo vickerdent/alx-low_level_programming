c66573316a03054aa42224a28d3f01a83b8f253d
Victor Abuka <vickerdent@gmail.com>
2023-02-17 13:56:40 +0000

    Numbers only in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c = 0;
+while (c < 10)
+{
+printf("%d", c);
+c++;
+}
+printf("\n");
+return (0);
+}

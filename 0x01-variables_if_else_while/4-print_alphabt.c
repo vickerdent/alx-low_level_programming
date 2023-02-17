27f2a041f59df4e35e3ed8d72ffc7d19ce4a8e5e
Victor Abuka <vickerdent@gmail.com>
2023-02-17 13:44:26 +0000

    Exclude 2 letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c = '\n';
+char ch = 'a';
+while (ch <= 'z')
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+ch++;
+}
+putchar(c);
+return (0);
+}

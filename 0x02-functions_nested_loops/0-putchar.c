ab85bd96167e6faf723133c77925d9d4b9121a90
Victor Abuka <vickerdent@gmail.com>
2023-02-21 18:04:44 +0000

    Fuction using putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char state[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
+int i;
+for (i = 0; i < 9; i++)
+_putchar(state[i]);
+return (0);
+}

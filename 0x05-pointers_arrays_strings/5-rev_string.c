fcb4e56ea6d66d0b7381db0e8727cf6d357f53ee
Victor Abuka <vickerdent@gmail.com>
2023-02-27 21:13:44 +0000

    Correction

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,11 +9,16 @@
 
 void rev_string(char *s)
 {
-int len = 0;
+int len = 0, i = 0;
+char aux;
 
 while (s[len] != '\0')
 len++;
-while (len)
-_putchar(s[--len]);
-_putchar('\n');
+
+while (i < len--)
+{
+aux = s[i];
+s[i++] = s[len];
+s[len] = aux;
+}
 }

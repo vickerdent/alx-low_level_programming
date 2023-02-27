a0d3938ed14ec07e58cc240d972ac885e069d89e
Victor Abuka <vickerdent@gmail.com>
2023-02-27 20:27:25 +0000

    Print my string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - a function that prints a string
+ * @str: An string input
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+puts(str);
+}

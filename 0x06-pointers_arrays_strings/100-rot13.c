f2a9d601fac0960145666267f998330db8557a1f
Victor Abuka <vickerdent@gmail.com>
2023-03-02 22:49:48 +0000

    ROT13 encryption

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *rot13 - function that encodes a string using rot13
+ * @s: A char input pointer
+ * Return: a pointer.
+ */
+
+char *rot13(char *)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+while ((s[i] >= 'a' && s[i] <= 'z') ||
+(s[i] >= 'A' && s[i] <= 'Z'))
+{
+if ((s[i] >= 'a' && s[i] <= 'm') ||
+(s[i] >= 'A' && s[i] <= 'M'))
+s[i] += 13;
+else
+s[i] -= 13;
+i++;
+}
+i++;
+}
+return (s);
+}
